171b64e1b9228271535297ae244a09a782184792
zarah <devvick230385@gmail.com>
2024-07-23 18:07:23 +0100

    UPDATE: completed hooops factory

---
+++ src/Hooops.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+import "./Community.sol";
+
+contract Hooops {
+    mapping(string => address) community;
+
+    function getContract(
+        string memory communityName
+    ) external view returns (address) {
+        address res = community[communityName];
+        require(res != address(0), "COMMUNITY_NOT_INITIALIZED");
+        return community[communityName];
+    }
+
+    function createCommunity(string memory name, uint initialReward) external {
+        require(community[name] == address(0), "COMMUNITY_ALREADY_EXIST");
+        require(initialReward > 0, "REWARD_CANNOT_BE_ZERO");
+        Community new_community = new Community(
+            name,
+            msg.sender,
+            initialReward
+        );
+        community[name] = address(new_community);
+    }
+}
