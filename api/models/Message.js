ade97d6080477326e4872c0bca943db1ff858c46
zarah <devvick230385@gmail.com>
2024-07-28 22:17:38 +0100

    RESOLVE: resolved null value on message id column

--- api/models/Message.js
+++ api/models/Message.js
@@ -16,6 +16,7 @@ module.exports = (sequelize, DataTypes) => {
       id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
+        autoIncrement: true,
       },
       message_id: {
         type: DataTypes.STRING,
